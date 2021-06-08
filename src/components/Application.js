40d32821ecb3966ad7c29d77cacfe652be59e907
Khea Chang <kheacyc@gmail.com>
2021-06-09 05:10:05 +0800

    built InterviewerListItem component

--- src/components/Application.js
+++ src/components/Application.js
@@ -39,7 +39,8 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-        <DayList days={days} day={day} setDay={setDay} />
+
+        <DayList days={days} day={day} setDay={setDay} /> /* props*/ 
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,29 @@
+import React from "react"
+import classNames from 'classnames';
+import "components/InterviewerListItem.scss"
+
+export default function InterviewerListItem(props) {
+
+  const interviewerClass = classNames(
+    'interviewers__item', {
+      'interviewers__item-image': props.src,
+      'interviewers__item--selected': props.selected,
+    }
+  )
+  return (<li className={interviewerClass} onClick = {() => {props.setInterviewer(props.name)}}>
+  <img
+    className={interviewerClass}
+    src="https://i.imgur.com/LpaY82x.png"
+    alt="Sylvia Palmer"
+  />
+  Sylvia Palmer
+</li>
+)
+}
+// Our InterviewerListItem component takes in the following props:
+
+// id:number - the id of the interviewer
+// name:string - the name of the interviewer
+// avatar:url - a url to an image of the interviewer
+// selected:boolean - to determine if an interview is selected or not
+// setInterviewer:function - sets the interviewer upon selection
